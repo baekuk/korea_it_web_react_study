19a0af1b73a4cd30b8335e91bfc5e1cb6502c853
baekuk <choiyl1@naver.com>
2025-08-06 21:42:09 +0900

    Init: first commit

---
+++ src/components/BasicProps/Props2/Props2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Props2({ a, b, ...나머지 }) {
+    console.log(나머지);
+    return (
+        <div>
+            <p>{a}</p>
+            <p>{b}</p>
+        </div>
+    );
+}
+
+export default Props2
